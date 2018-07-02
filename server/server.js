4bfadddd99834b076793ccf93a4e2c1fdde1fcc3
RedXylo <red_xylo@hotmail.com>
2018-07-01 22:21:50 -0400

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -4,11 +4,6 @@ var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js'); 
 var {User} = require('./models/user.js'); 
 
-beforeEach((done) => {
-    Todo.remove({}).then(() => {
-        done(); 
-    });
-});
 var app = express(); 
 
 app.use(bodyParser.json()); 
@@ -23,6 +18,14 @@ app.post('/todos', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+app.get('/todos',(req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e); 
+    });
+}); 
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
